d126eb6aa2237fb65ebadbdad5311a7661e56040
thanhbinh030301 <61733464+thanhbinh030301@users.noreply.github.com>
2022-01-11 16:18:38 +0700

    Update page and function

--- OrderFoodApp/OrderFoodApp/App.xaml.cs
+++ OrderFoodApp/OrderFoodApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace OrderFoodApp
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new TabPage());
+            MainPage = new NavigationPage(new MainPage());
             
         }
 
